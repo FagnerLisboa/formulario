77cc91a309fd8b3e293ad268b166da1119e6221d
Fagner Lisboa <fnlisboa2016@gmail.com>
2022-03-26 01:21:05 -0300

    Formulario em TypeScript

---
+++ src/app/clientes/formulario/formulario.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../shared/cliente';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.scss']
+})
+export class FormularioComponent implements OnInit {
+  formCliente!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.createForm(new Cliente());
+  }
+
+  createForm(cliente: Cliente) {
+    this.formCliente = new FormGroup({
+      fullnome: new FormControl(cliente.fullname),
+      login: new FormControl(cliente.login),
+      password: new FormControl(cliente.password),
+      email: new FormControl(cliente.email),
+      cell: new FormControl(cliente.cell),
+          
+    })
+  }
+  
+  onSubmit() {
+    console.log(this.formCliente.value);
+    this.formCliente.reset(new Cliente());
+
+ }
+}
---
+++ src/app/clientes/shared/cliente.ts
@@ -0,0 +1,7 @@
+export class Cliente {
+    fullname: string = '';
+    login: string = '';
+    password: string = '';
+    email: string = '';
+    cell: number = 1;
+  }
